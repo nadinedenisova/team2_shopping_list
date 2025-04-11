a66c5ee41312fe9a1a7b29347c7bd60401fbab7a
SergeyBoykov <sutemi67@gmail.com>
2025-04-11 20:09:18 +0300

    dependencies

--- app/src/main/java/acr/appcradle/shoppinglist/MainActivity.kt
+++ app/src/main/java/acr/appcradle/shoppinglist/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import acr.appcradle.shoppinglist.ui.theme.ShoppingListTheme
+import androidx.compose.material3.ElevatedButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,8 @@ dependencyResolutionManagement {
     }
 }
 
+
+
 rootProject.name = "ShoppingList"
 include(":app")
  
